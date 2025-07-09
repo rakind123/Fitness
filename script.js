3bbc38cb06a913ffb9007c0165b71a980cfd89d2
rakind123 <rakindmann.3@gmail.com>
2025-07-09 14:12:54 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-function showSection(sectionId) {
-  const sections = ['exercise', 'meals', 'tracker'];
-  sections.forEach(id => document.getElementById(id)?.classList.add('hidden'));
-  document.getElementById(sectionId)?.classList.remove('hidden');
-}
-
-/* Additional script content omitted for brevity */
+console.log('Script loaded');
